5885d5494f0673e751096a5c912df75cee31ba90
oabouhajar <smsm.sy@hotmail.com>
2020-02-07 11:50:10 +0000

    final vserion of the project

--- ca2.erl
+++ ca2.erl
@@ -8,6 +8,55 @@
 %%%-------------------------------------------------------------------
 
 
+
+%***************************** Project node map Structure
+
+%                       rad                      joe
+%                       *                       *
+%                       *                     *
+%                       *                   *
+%        jack  ******  sam ************ osama
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%***********************************  TSETING steps
+
+%%%%%%%%    Strting two shell sam and osama
+%           1-          erl -sname osama
+%           2-          erl -sname sam
+
+
+%%%%%%%%    Start the processes and register them as (otest, stest)  *******
+%           3- run the below line on the shells as decriped
+%           osama >     c(ca2). PID = ca2:start(osama). register(otest,PID).
+%           sam >       c(ca2). PID = ca2:start(sam). register(stest,PID).
+
+
+
+%%%%%%%%    4- Send neighbours List to each others 
+%           osama >     {stest,sam@osama}!{updateNL,osama,PID}. 
+%           sam >       {otest,osama@osama}!{updateNL,sam,PID}.
+
+
+%%%%%%%%    Send compute Msg to from (osama -> sam) to get the Nth prim to (sam) distination
+%           osama >     {stest,sam@osama}!{computeNthPrime,5,sam,osama,1}. 
+
+
+%% ******   check sam shell to see the results
+
+
+
+ %%%%%% to test the update Route Table msg
+
+%        osama >        RTable = [{osama,osama,0},{sam,sam,1},{joe,joe,1},{jack,sam,2},{rad,sam,2}].
+%        osama >        NewRTable = [{sam,sam,0},{osama,osama,1},{jack,jack,1},{rad,rad,1},{joe,osama,2}].
+%        osama >        {stest,sam@osama}!{updateRT,RTable,NewRTable,osama}. 
+
+
+%%%%%%%%%%%%% DONE %%%%%%%%%%%%
+
+
 -module(ca2).
 -compile(export_all).
 
@@ -17,32 +66,18 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%          Project init values section      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-initNList(Name)->
-    NList = [],
-    NList.
-
-nListOsama()->
-    PJoe = spawn(fun()-> loop(joe,[],[]) end),
-    NList = [{joe,PJoe},{sam,xxx}],
-    NList.
-
-rTableOsama()->
+%%%%%%%%%%%%%%% ****************** two process will be created for testing
+initOsama()->
     RTable = [{osama,osama,0},{sam,sam,1},{joe,joe,1},{jack,sam,2},{rad,sam,2}],
-    RTable.
+    PID = spawn(fun()->ca2:loop(osama,RTable,[]) end),
+    PID ! {updateNL,osama,PID},
+    PID.
 
-nListSam()->
-    PJack = spawn(fun()-> loop(jack,[],[]) end),
-    PRad = spawn(fun()-> loop(rad,[],[]) end),
-
-    NList = [{jack,PJack},{rad,PRad},{osama,xxx}],
-    NList.
-
-rTableSam()->
+initSam()->
     RTable = [{sam,sam,0},{osama,osama,1},{jack,jack,1},{rad,rad,1},{joe,osama,2}],
-    RTable.
-
-
+    PID = spawn(fun()->ca2:loop(sam,RTable,[]) end),
+    PID ! {updateNL,sam,PID},
+    PID.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    Getting the Prime number section      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -114,10 +149,12 @@ lookUpTable(X,[_|Tail]) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
         %********************         Update neighbours List               ***************%
-
-
-
-
+updateNL(Name,[],PID)->
+    [{Name,PID}];
+updateNL(Name,[{Name,_}|Tail],PID)->
+    [{Name,PID}|Tail];
+updateNL(Name,[Head|Tail],PID)->
+    [Head|updateNL(Name,Tail,PID)].
 
         %********************         Find neighbours PID               ***************%
 findNighPid(_,[])->
@@ -127,38 +164,25 @@ findNighPid(X,[{D,N}|_]) when X == D ->
 findNighPid(X,[_|Tail]) ->
     findNighPid(X,Tail).
 
-updateNL(Name,[],PID)->
-    [{Name,PID}];
-updateNL(Name,[{Name,_}|Tail],PID)->
-    [{Name,PID}|Tail];
-updateNL(Name,[Head|Tail],PID)->
-    [Head|updateNL(Name,Tail,PID)].
-    
-
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%          Project Main section              %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
 hello(N)->
     self() ! {sendHello,N}.
 
-%% start the project and return the process ID started
-uNL(Name,RTable,NList)->
-    PID = spawn(fun()->loop(Name,RTable,NList) end),
-    NewNL = updateNL(Name,NList,PID),
-    %uNL(Name,RTable,NList,PID, NewNL),
-    PID.
-
 
 start(PName)->
-    RT = ca2:rTableOsama(),
-    PID = spawn(fun()->ca2:loop(PName,RT,[]) end),
-    PID ! {updateNL,PName,PID},
+case PName of
+    osama ->
+        PID  = ca2:initOsama();
+    sam -> 
+        PID  = ca2:initSam();
+    _ -> 
+        PID  = spawn(fun()->ca2:loop(PName,[],[]) end),
+        PID ! {updateNL,PName,PID}
+    end,
     PID.
-
 
 loop(Name,RTable,NList)->
     receive
@@ -167,57 +191,87 @@ loop(Name,RTable,NList)->
             io:fwrite(" *********** Update the neighbours List by : ~p~n", [SenderName]),
             io:fwrite(" >>>>>>>  the NEW neighbours List  is ~p~n ", [NewNL] ),
                 loop(Name,RTable,NewNL);
+        {updateRT,RT,NewRTable,Sender} ->
+            NewRT = updateRT(RT,NewRTable,Sender),
+            io:fwrite(" *********** Update the Route Table by : ~p~n", [Sender]),
+            io:fwrite(" >>>>>>>>>>  The NEW Route Table is    : ~p~n ", [NewRT] ),
+                loop(Name,NewRTable,NewRT);
         {receiveAnswer,Ans,Index,DistNname,Sender}->
-            io:fwrite("the Ans  is ~p~n", [Ans] ),
-            io:fwrite("the Index  is ~p~n", [Index] ),
-            io:fwrite("the DistNname  is ~p~n", [DistNname] ),
-            io:fwrite("the Sender  is ~p~n", [Sender] ),
+            io:fwrite("********* message received from ******** ~p~n" , [Sender] ),
+            io:fwrite("the Index requested        is ~p~n", [Index] ),
+            io:fwrite("the Answer                 is ~p~n", [Ans] ),
+            io:fwrite("the destination Nickname   is ~p~n", [DistNname] ),
                 loop(Name,NList,RTable);
         {computeNthPrime,_,_,_,15}->
                 loop(Name,NList,RTable);
-        {computeNthPrime,Index,Dist,Sender,Hops}->
+        {computeNthPrime,Index,Dist,Sender,_}->
             Ans = getNthPrime(Index),
             Neigh = lookUpTable(Dist,RTable),
             NPid =  findNighPid(Neigh,NList),
             NPid!{receiveAnswer,Ans,Index,Dist,Sender},
                 loop(Name,NList,RTable);
+        {getRT,Sender}->
+            Pid =  findNighPid(Sender,NList),
+            Pid ! RTable,             
+                loop(Name,NList,RTable);
+        {getNl,Sender}->
+            Pid =  findNighPid(Sender,NList),
+            Pid ! NList, 
+                loop(Name,NList,RTable);    
         {sendHello,DistNname}->
             NPid =  findNighPid(DistNname,NList),
-            self()! io:fwrite("Hello Sent To  ~p~n", [DistNname]),
+            self()! io:fwrite("Hello Sent for test To  ~p~n", [DistNname]),
             NPid ! {helloMsg, Name},
                 loop(Name,RTable,NList);
         {helloMsg, SenderName}->
-            io:fwrite("Hello From  ~p~n", [SenderName]),
+            io:fwrite("Hello test From  ~p~n", [SenderName]),
                 loop(Name,RTable,NList)
 end.
 
 
+%***************************** Project node map Structure
+
+%                       rad                      joe
+%                       *                       *
+%                       *                     *
+%                       *                   *
+%        jack  ******  sam ************ osama
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%***********************************  TSETING steps
+
+%%%%%%%%    Strting two shell sam and osama
+%           1-          erl -sname osama
+%           2-          erl -sname sam
+
+
+%%%%%%%%    Start the processes and register them as (otest, stest)  *******
+%           3- run the below line on the shells as decriped
+%           osama >     c(ca2). PID = ca2:start(osama). register(otest,PID).
+%           sam >       c(ca2). PID = ca2:start(sam). register(stest,PID).
 
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%% TSETING 
-% ****      start the processes ********
-
-%         c(ca2). PID = ca2:start(osama). register(otest,PID).
-%         c(ca2). PID = ca2:start(sam). register(stest,PID).
-% 
-%       ******************* send NL to each others ***********
-%       {stest,sam@osama}!{updateNL,osama,PID}. 
-%       {otest,osama@osama}!{updateNL,sam,PID}.
-
-%       {stest,sam@osama}!{computeNthPrime,5,sam,osama,1}. 
+%%%%%%%%    4- Send neighbours List to each others 
+%           osama >     {stest,sam@osama}!{updateNL,osama,PID}. 
+%           sam >       {otest,osama@osama}!{updateNL,sam,PID}.
 
 
+%%%%%%%%    Send compute Msg to from (osama -> sam) to get the Nth prim to (sam) distination
+%           osama >     {stest,sam@osama}!{computeNthPrime,5,sam,osama,1}. 
 
-% c(ca2). RT = ca2:rTableOsama(). LN =ca2:nListOsama(). PID = spawn(fun()->ca2:loop(osama,RT,LN) end). register(otest ,PID).  ca2:uNNL(osama,RT,LN,PID).
 
-%{sam,sam@osama}!{updateNL,osama,RT,LN,self(),xxx}.
-% otest ! {updateNL,osama,RT,LN,PID}.
+%% ******   check sam shell to see the results
 
-%  {stest,sam@osama}!{updateNL,osama,PID}. 
 
-%  {stest,sam@osama}!{computeNthPrime,5,sam,osama,1}. 
 
-% c(ca2). RT = ca2:rTableSam(). LN =ca2:nListSam(). PIDS = spawn(fun()->ca2:loop(sam,RT,LN) end). register(stest,PIDS). {osama,osama@osama}!{updateNL,sam,RTS,LNS,self(),xxx}.
-% {otest,osama@osama}!{updateNL,sam,PIDS}.
+ %%%%%% to test the update Route Table msg
 
+%        osama >        RTable = [{osama,osama,0},{sam,sam,1},{joe,joe,1},{jack,sam,2},{rad,sam,2}].
+%        osama >        NewRTable = [{sam,sam,0},{osama,osama,1},{jack,jack,1},{rad,rad,1},{joe,osama,2}].
+%        osama >        {stest,sam@osama}!{updateRT,RTable,NewRTable,osama}. 
+
+
+%%%%%%%%%%%%% DONE %%%%%%%%%%%%
